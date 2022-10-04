e7fed4949b27195e93f34c55591c1200415ee9ac
maria <mariyagrabko@gmail.com>
2022-10-04 12:51:31 +1000

    Create method InputArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InputArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Input the element of the massive: ");
+        array[i] = Console.ReadLine();
+       
+    }
+     return array;
+}
+
+Console.Write("Input the size of the massive: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] newArray = InputArray(size);
